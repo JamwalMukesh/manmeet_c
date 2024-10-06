49a458d36e23f7d90582d874b3ed03f97ac41271
MukeshJamwal <mukeshjamwal@trigvent.com>
2024-10-06 13:14:31 +0530

    first commit

---
+++ 1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    printf(" Hello Good Morning \n");
+    printf(" Hello Good \n Morning ");
+    printf(" Hello Good\t Morning ");
+    printf(" Hello \"Good\" \'Morning\' ");
+    printf(" Hello\b\b Good Morning \n");
+    printf(" Magic Happens \r Here\n");
+    // " Herec Happens"
+    return 0;
+}
